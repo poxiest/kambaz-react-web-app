6b1f87d5002b54d1b7b81a2bffe6b7b4a150c885
Sri Vishaak Ramesh Babu <rameshbabu.s@northeastern.edu>
2025-01-14 17:26:49 -0500

    Until 1.3.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Labs from "./Labs";
+import Kambaz from "./Kambaz";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Navigate to="Labs" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
         </Routes>
       </div>
     </HashRouter>
