b3f5f7df7374aa91c6ca8d16e5b71e0f49520ee2
Sri Vishaak Ramesh Babu <rameshbabu.s@northeastern.edu>
2025-01-17 16:22:06 -0500

    UNTIL 1.3.7 Assignments Screen (On Your Own)

--- src/Kambaz/Courses/index.tsx
+++ src/Kambaz/Courses/index.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Modules from "./Modules";
 import CourseNavigation from "./Navigation";
 import Home from "./Home";
+import Assignments from "./Assignments";
 export default function Courses() {
     return (
         <div id="wd-courses">
@@ -17,7 +18,7 @@ export default function Courses() {
                             <Route path="/" element={<Navigate to="Home" />} />
                             <Route path="Home" element={<Home />}/>
                             <Route path="Modules" element={<Modules />} />
-                            <Route path="Assignments" element={<h2>Assignments</h2>} />
+                            <Route path="Assignments" element={<Assignments />} />
                             <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
                             <Route path="People" element={<h2>People</h2>} />
                         </Routes>
