c6fcdfcfee7d1ed47ec55ee1ae16fff9a3c424a7
Sri Vishaak Ramesh Babu <rameshbabu.s@northeastern.edu>
2025-01-31 17:04:05 -0500

    Revert "Until 2.3.18"
    This reverts commit 2809a67f5422a6b5e7b422678020e2c42b869ff9.

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,36 +1,14 @@
-import { Nav } from "react-bootstrap";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function TOC() {
     return (
-        // <ul>
-        //     <li><Link to="/Labs">Labs</Link></li>
-        //     <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        //     <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        //     <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        //     <li><Link to="/Kambaz">Kambaz</Link></li>
-        //     <li><a href="https://github.com/poxiest/kambaz-react-web-app" id="wd-github">Github Link</a></li>
-        // </ul>
-        <Nav variant="pills">
-            <Nav.Item>
-                <Nav.Link href="#/Labs">Labs</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link href="https://github.com/poxiest/kambaz-react-web-app">My GitHub</Nav.Link>
-            </Nav.Item>
-        </Nav>
-
+        <ul>
+            <li><Link to="/Labs">Labs</Link></li>
+            <li><Link to="/Labs/Lab1">Lab 1</Link></li>
+            <li><Link to="/Labs/Lab2">Lab 2</Link></li>
+            <li><Link to="/Labs/Lab3">Lab 3</Link></li>
+            <li><Link to="/Kambaz">Kambaz</Link></li>
+            <li><a href="https://github.com/poxiest/kambaz-react-web-app" id="wd-github">Github Link</a></li>
+        </ul>
     );
 }
