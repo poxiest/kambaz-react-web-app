2809a67f5422a6b5e7b422678020e2c42b869ff9
Sri Vishaak Ramesh Babu <rameshbabu.s@northeastern.edu>
2025-01-31 16:40:39 -0500

    Until 2.3.18

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -11,9 +11,17 @@ import Zindex from "./Zindex";
 import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
+import ReactIconsSampler from "./ReactIcons";
+import { Container } from "react-bootstrap";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container id="wd-lab2">
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <div id="wd-css-id-selectors">
@@ -65,5 +73,12 @@ export default function Lab2() {
       <Float />
       <GridLayout />
       <Flex />
-    </div>);
+      <ReactIconsSampler />
+      <BootstrapGrids />
+      <ScreenSizeLabel />
+      <BootstrapTables />
+      <BootstrapLists />
+      <BootstrapForms />
+      <BootstrapNavigation />
+    </Container>);
 }
