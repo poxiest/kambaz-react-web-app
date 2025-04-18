8e9d8c588c5517f4c8e2dc7d473b6712c27f9629
vaarunx <varunrishwandh2001@gmail.com>
2025-04-17 22:57:32 -0400

    Added all the quiz files

--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -16,12 +16,17 @@ export const deleteCourse = async (id: string) => {
 };
 
 export const updateCourse = async (course: any) => {
-  const { data } = await axiosWithCredentials.put(`${COURSES_API}/${course._id}`, course);
+  const { data } = await axiosWithCredentials.put(
+    `${COURSES_API}/${course._id}`,
+    course
+  );
   return data;
 };
 
 export const findModulesForCourse = async (courseId: string) => {
-  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/modules`);
+  const response = await axiosWithCredentials.get(
+    `${COURSES_API}/${courseId}/modules`
+  );
   return response.data;
 };
 
@@ -34,24 +39,43 @@ export const createModuleForCourse = async (courseId: string, module: any) => {
 };
 
 export const findAssignmentsForCourse = async (courseId: string) => {
-  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/assignments`);
+  const response = await axiosWithCredentials.get(
+    `${COURSES_API}/${courseId}/assignments`
+  );
   return response.data;
 };
 
-export const createAssignmentForCourse = async (courseId: string, assignment: any) => {
+export const createAssignmentForCourse = async (
+  courseId: string,
+  assignment: any
+) => {
   const response = await axiosWithCredentials.post(
     `${COURSES_API}/${courseId}/assignments`,
     assignment
   );
   return response.data;
-}
+};
 
 export const createCourse = async (course: any) => {
   const { data } = await axiosWithCredentials.post(COURSES_API, course);
   return data;
- }; 
+};
 
 export const findUsersForCourse = async (courseId: any) => {
   const response = await axios.get(`${COURSES_API}/${courseId}/users`);
+  return response.data;
+};
+
+// Getting quizzes for the course
+export const findQuizzesForCourse = async (courseId: any) => {
+  const response = await axiosWithCredentials.get(
+    `${COURSES_API}/${courseId}/Quizzes`
+  );
+  return response.data;
+};
+
+// Creating new quizzes for the course
+export const createQuizForCourse = async (courseId: any, quiz: any) => {
+  const response = await axios.post(`${COURSES_API}/${courseId}/Quizzes`, quiz);
   return response.data;
 };
