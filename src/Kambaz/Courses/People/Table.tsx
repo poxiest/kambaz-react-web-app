80804afdc1888799b113e7460e8bbdde04b8569b
poxiest <vishaak98@gmail.com>
2025-04-20 15:35:26 -0400

    added txt file

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Table } from "react-bootstrap";
 import { useParams } from "react-router-dom";
